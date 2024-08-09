96a1a63d2a2e6076d44a251eeea12baf5b6a617f
EdwinHn <162158782+EdwinHn@users.noreply.github.com>
2024-08-09 08:24:38 -0600

    Ejercicio 1- Palabra Alreves

--- Hernandez_Edwin_Prueba1.java
+++ Hernandez_Edwin_Prueba1.java
@@ -22,7 +22,7 @@ public class Hernandez_Edwin_Prueba1 {
         do {
             int cicloOpcion = 1;
 
-            System.out.println("--- MENNU ---\n");
+            System.out.println("--- MENU ---\n");
             System.out.println("1- Palabra Alreves");
             System.out.println("2- Número Perfecto");
             System.out.println("3- Primos");
@@ -38,23 +38,49 @@ public class Hernandez_Edwin_Prueba1 {
                     System.out.print("Ingrese una opción válida: ");
 
                 } else if (opcion == 1) {
-                    System.out.println("** Palabra Alreves **\n");
+                    
+                    int contador=0;
+                    String palabra;
+                    String palabraInvertida = "";
+
+                    System.out.println("\n** Palabra Alreves **\n");
+
+                    System.out.print("Ingrese una palabra: ");
+                    palabra = scan.next();
+
+                    int length = (palabra.length());
+
+                    //Inversion del texto para su impresion posterior
+                    for (int contadorInvertido = 0; contadorInvertido < (palabra.length()); contadorInvertido++) {
+                        char letraInvertida = palabra.charAt(contadorInvertido);
+                        palabraInvertida = letraInvertida + palabraInvertida;
+
+                    }
+                    
+                    while (length>0){
+                        System.out.print(palabraInvertida.charAt(contador));
+
+                    contador++;
+                    length--;
+                    }
+
+                    System.out.println("\n");
                     cicloOpcion = 0;
 
                 } else if (opcion == 2) {
-                    System.out.println("** Número perfecto **\n");
+                    System.out.println("\n** Número perfecto **\n");
                     cicloOpcion = 0;
 
                 } else if (opcion == 3) {
-                    System.out.println("** Primos **\n");
+                    System.out.println("\n** Primos **\n");
                     cicloOpcion = 0;
 
                 } else if (opcion == 4) {
-                    System.out.println("** Votaciones **\n");
+                    System.out.println("\n** Votaciones **\n");
                     cicloOpcion = 0;
 
                 } else if (opcion == 5) {
-                    System.out.println("adios :)");
+                    System.out.println("\nadios :)");
                     cicloOpcion = 0;
                     menu = 0;
                 }
